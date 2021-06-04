08b53266240673f032c1a5d160414f3f2d316999
Ryan Farmer <ryanfarmer@mac.com>
2021-06-03 22:55:01 -0400

    Seeding Initial Options (Billing, Filter, 3rd Party)

---
+++ Migrations/20210603231216_SeedBillingPeriods.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prototype.Migrations
+{
+    public partial class SeedBillingPeriods : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Billing Period Inserts
+            migrationBuilder.Sql("INSERT INTO BillingPeriod (Name) VALUES ('Monthly')");
+            migrationBuilder.Sql("INSERT INTO BillingPeriod (Name) VALUES ('Annual')");
+
+            // Media Filter Inserts
+            migrationBuilder.Sql("INSERT INTO MediaFilters (Size) VALUES ('14x14x1')");
+            migrationBuilder.Sql("INSERT INTO MediaFilters (Size) VALUES ('21x21x1')");
+
+            // Other Warranties
+            migrationBuilder.Sql("INSERT INTO OtherWarranties (Name) VALUES ('None')");
+            migrationBuilder.Sql("INSERT INTO OtherWarranties (Name) VALUES ('WeFixinNothin')");
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
