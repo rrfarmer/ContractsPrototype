3a337d1555a24fbb6c82008989a49b8f3759dd17
rrfarmer <ryanfarmer@mac.com>
2021-08-26 14:08:56 -0400

    Changed to non preview version and fixed EF Migration for new DB setup
    Probably need to do some a new migration for larger test data

--- Migrations/20210604043349_AddTempTestContract.cs
+++ Migrations/20210604043349_AddTempTestContract.cs
@@ -7,8 +7,8 @@ namespace Prototype.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             // Create a couple Contracts for Customer 1
-            migrationBuilder.Sql("INSERT INTO Contracts (CustomerId, Address, City, State, Zip, StartDate, BillingPeriodId, isActive, OtherWarrantyId) VALUES (4, '123 Left North St', 'Altgard', 'IO', 55555, '1/1/2021 12:00:00 AM', 1, 1, 2)");
-            migrationBuilder.Sql("INSERT INTO Contracts (CustomerId, Address, City, State, Zip, StartDate, BillingPeriodId, isActive, OtherWarrantyId) VALUES (4, '123 Right North St', 'Altgard', 'IO', 55555, '3/1/2020 12:00:00 AM', 1, 0, 1)");
+            migrationBuilder.Sql("INSERT INTO Contracts (CustomerId, Address, City, State, Zip, StartDate, BillingPeriodId, isActive, OtherWarrantyId) VALUES (1, '123 Left North St', 'Altgard', 'IO', 55555, '1/1/2021 12:00:00 AM', 1, 1, 2)");
+            migrationBuilder.Sql("INSERT INTO Contracts (CustomerId, Address, City, State, Zip, StartDate, BillingPeriodId, isActive, OtherWarrantyId) VALUES (1, '123 Right North St', 'Altgard', 'IO', 55555, '3/1/2020 12:00:00 AM', 1, 0, 1)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
--- Migrations/20210604043548_AddTempTestUnitsVisits.cs
+++ Migrations/20210604043548_AddTempTestUnitsVisits.cs
@@ -9,12 +9,12 @@ namespace Prototype.Migrations
             // Add a couple Units to Contract 1 and 2
             migrationBuilder.Sql("INSERT INTO Units (MediaFilterId, ContractId) VALUES (1, 2)");
             migrationBuilder.Sql("INSERT INTO Units (MediaFilterId, ContractId) VALUES (1, 2)");
-            migrationBuilder.Sql("INSERT INTO Units (MediaFilterId, ContractId) VALUES (1, 3)");
+            migrationBuilder.Sql("INSERT INTO Units (MediaFilterId, ContractId) VALUES (1, 1)");
 
             // Add a couple Service Visits to a contract
             migrationBuilder.Sql("INSERT INTO ServiceVisit (Visit, ContractId) VALUES ('1/1/2021 12:00:00 AM', 2)");
             migrationBuilder.Sql("INSERT INTO ServiceVisit (Visit, ContractId) VALUES ('1/1/2021 12:00:00 AM', 2)");
-            migrationBuilder.Sql("INSERT INTO ServiceVisit (Visit, ContractId) VALUES ('1/1/2021 12:00:00 AM', 3)");
+            migrationBuilder.Sql("INSERT INTO ServiceVisit (Visit, ContractId) VALUES ('1/1/2021 12:00:00 AM', 1)");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
